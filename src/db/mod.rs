99c4cf316cafa023454990845c4e83c1d1021f77
Dromader2137 <wojciechowski.ignacy@proton.me>
2024-11-24 19:38:35 +0100

    refactor

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -27,9 +27,85 @@ async fn create_user_table(db: &mut SqliteConnection) -> Result<(), String> {
         .execute(db).await {
         Err(err) => {
             if &format!("{}", err) == "error returned from database: (code: 1) table users already exists" {
+                return Ok(())
+            } else {
+                return Err(format!("Failed to create users table: {}", err))
+            }
+        },
+        _ => Ok(())
+    }
+    
+}
+
+async fn create_verification_table(db: &mut SqliteConnection) -> Result<(), String> {
+    if let Ok(var) = env::var("WIEDZIELISCIE_BACKEND_RESET_DB") {
+        if var.to_lowercase() == "true" || var == "1" {
+            query("DROP TABLE verifications").execute(&mut *db).await.ok();
+        }
+    }
+
+    match query("CREATE TABLE verifications (
+        user_id int,
+        timestamp int,
+        verification_token varchar(255)
+    )")
+        .execute(db).await {
+        Err(err) => {
+            if &format!("{}", err) == "error returned from database: (code: 1) table verifications already exists" {
                 Ok(())
             } else {
-                Err(format!("Failed to create users table: {}", err))
+                Err(format!("Failed to create verifications table: {}", err))
+            }
+        },
+        _ => Ok(())
+    }
+}
+
+async fn create_session_table(db: &mut SqliteConnection) -> Result<(), String> {
+    if let Ok(var) = env::var("WIEDZIELISCIE_BACKEND_RESET_DB") {
+        if var.to_lowercase() == "true" || var == "1" {
+            query("DROP TABLE sessions").execute(&mut *db).await.ok();
+        }
+    }
+
+    match query("CREATE TABLE sessions (
+        user_id int,
+        session_token varchar(255),
+        timestamp int,
+        valid_until int
+    )")
+        .execute(db).await {
+        Err(err) => {
+            if &format!("{}", err) == "error returned from database: (code: 1) table sessions already exists" {
+                Ok(())
+            } else {
+                Err(format!("Failed to create sessions table: {}", err))
+            }
+        },
+        _ => Ok(())
+    }
+}
+
+async fn create_password_reser_table(db: &mut SqliteConnection) -> Result<(), String> {
+    if let Ok(var) = env::var("WIEDZIELISCIE_BACKEND_RESET_DB") {
+        if var.to_lowercase() == "true" || var == "1" {
+            query("DROP TABLE password_resets").execute(&mut *db).await.ok();
+        }
+    }
+
+    match query("CREATE TABLE password_resets (
+        user_id int,
+        reset_token varchar(255),
+        password varchar(255), 
+        timestamp int,
+        valid_until int
+    )")
+        .execute(db).await {
+        Err(err) => {
+            if &format!("{}", err) == "error returned from database: (code: 1) table password_resets already exists" {
+                Ok(())
+            } else {
+                Err(format!("Failed to create password_resets table: {}", err))
             }
         },
         _ => Ok(())
@@ -38,6 +114,18 @@ async fn create_user_table(db: &mut SqliteConnection) -> Result<(), String> {
 
 pub async fn create_tables(mut db: PoolConnection<Sqlite>) {
     if let Err(err) = create_user_table(&mut db).await {
+        panic!("{}", err);
+    }
+    
+    if let Err(err) = create_verification_table(&mut db).await {
+        panic!("{}", err);
+    }
+    
+    if let Err(err) = create_session_table(&mut db).await {
+        panic!("{}", err);
+    }
+    
+    if let Err(err) = create_password_reser_table(&mut db).await {
         panic!("{}", err);
     }
 }
--- src/user/reset.rs
+++ src/user/reset.rs
@@ -1,3 +1,6 @@
+use std::env;
+
+use resend_rs::{types::CreateEmailBaseOptions, Resend};
 use rocket::{http::Status, response::content::RawHtml, serde::{json::{json, Json, Value}, Deserialize}};
 use rocket_db_pools::Connection;
 
@@ -12,6 +15,34 @@ pub struct ResetData<'r> {
     plaintext_password: &'r str,
 }
 
+async fn send_password_reset_email(email: &str, reset_token: &str) -> Result<(), String> {
+    let resend = Resend::default();
+
+    let from = match env::var("WIEDZIELISCIE_BACKEND_FROM_MAIL") {
+        Ok(val) => val,
+        Err(_) => return Err("From mail not found".to_owned()),
+    };
+    let subject = "Confirm your registration to WiedzieLIŚCIE";
+    let password_reset_link = match env::var("WIEDZIELISCIE_BACKEND_URL") {
+        Ok(val) => val + "/auth/password_reset/verify/" + reset_token,
+        Err(_) => return Err("Url not found".to_owned()),
+    };
+
+    let email = CreateEmailBaseOptions::new(from, [email], subject).with_html(&format!(
+        "
+            <a href=\"{}\">Click this to confirm password change</a>
+            <p>If the link above doesn't work just copy this and paste it into a new browser tab: {}</p>
+        ",
+        password_reset_link, password_reset_link
+    ));
+
+    if let Err(err) = resend.emails.send(email).await {
+        Err(format!("Failed to send email: {}", err))
+    } else {
+        Ok(())
+    }
+}
+
 #[post("/auth/password_reset", format = "json", data = "<data>")]
 pub async fn auth_password_reset(mut db: Connection<DB>, data: Json<ResetData<'_>>) -> (Status, Value) {
     let user = match get_user_by_email(&mut db, &data.email).await {
@@ -19,10 +50,8 @@ pub async fn auth_password_reset(mut db: Connection<DB>, data: Json<ResetData<'_
         Err(_) => return (Status::BadRequest, json!({"error": "User not found"}))
     };
 
-    if user.pending_password.is_some() {
-        return (Status::BadRequest, json!({"error": "Password reset already in progress"}));
-    }
     
+
     (Status::Ok, json!({}))
 }
 
