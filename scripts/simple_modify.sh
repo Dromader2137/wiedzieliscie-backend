c0d1dc6a8dd4e0c9b0ce013921b924bad5aaf351
Dromader2137 <wojciechowski.ignacy@proton.me>
2024-11-26 21:10:05 +0100

    script fix

--- scripts/simple_modify.sh
+++ scripts/simple_modify.sh
@@ -3,9 +3,9 @@
 path="$1/user/modify/$2"
 
 curl -s --location --request POST "$path" \
---header 'Content-Type: application/json' \
---data-raw '{
+  --header 'Content-Type: application/json' \
+  --data-raw '{
 	"jwt": "'"$3"'",
 	"new_value": "'"$4"'",
-	"account_id": "'"$5"'"
+	"account_id": '$5'
 }'
