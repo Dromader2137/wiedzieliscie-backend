fc0b3f59ce0d3729001375371e7c33a9f39e54cd
Feeeeddmmmeee <wislocki.janek@gmail.com>
2024-11-27 17:06:30 +0100

    added the deleted_users table

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -37,6 +37,41 @@ async fn create_user_table(db: &mut SqliteConnection) -> Result<(), String> {
     }
 }
 
+async fn create_deleted_user_table(db: &mut SqliteConnection) -> Result<(), String> {
+    if let Ok(var) = env::var("WIEDZIELISCIE_BACKEND_RESET_DB") {
+        if var.to_lowercase() == "true" || var == "1" {
+            query("DROP TABLE deleted_users").execute(&mut *db).await.ok();
+        }
+    }
+
+    match query(
+        "CREATE TABLE users (
+        user_id int,
+        first_name varchar(255),
+        last_name varchar(255),
+        email varchar(255),
+        password varchar(255),
+        gender bool,
+        verified bool,
+        admin bool
+        )",
+        )
+        .execute(db)
+        .await
+        {
+            Err(err) => {
+                if &format!("{}", err)
+                    == "error returned from database: (code: 1) table deleted_users already exists"
+                {
+                    Ok(())
+                } else {
+                    Err(format!("Failed to create deleted_users table: {}", err))
+                }
+            }
+            _ => Ok(()),
+        }
+}
+
 async fn create_verification_table(db: &mut SqliteConnection) -> Result<(), String> {
     if let Ok(var) = env::var("WIEDZIELISCIE_BACKEND_RESET_DB") {
         if var.to_lowercase() == "true" || var == "1" {
