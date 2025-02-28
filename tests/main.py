76f4a0ebdb84305634dd478a55d242a611540947
Dromader2137 <wojciechowski.ignacy@proton.me>
2025-02-28 19:20:04 +0100

    tests 1

--- tests/main.py
+++ tests/main.py
@@ -4,6 +4,7 @@ import subprocess
 import json
 import time
 import socket
+import sqlite3
 
 addr = ""
 port = 0
@@ -31,20 +32,34 @@ def expect(t, n, a, b):
         stop = True
 
 def test_register():
-    url = f"http://{addr}:{port}/auth/register"
-    data = {
+    register_url = f"http://{addr}:{port}/auth/register"
+    register_data = {
         "email": "user.mail.1@user.io",
         "plaintext_password": "user_1_passwd",
         "first_name": "user",
         "last_name": "number1",
         "gender": "m"
     }
-    register_request = requests.post(url, json=data)
+    register_request = requests.post(register_url, json=register_data)
     status = register_request.status_code
     response = json.loads(register_request.text)
     expect("register", "status", status, 201)
     expect("register", "account_id", response, {"account_id": 1})
+
+    global stop
+    if stop:
+        return
+   
+    conn = sqlite3.connect("db.sqlite")
+    cur = conn.cursor()
+
+    cur.execute("SELECT verification_token FROM verifications WHERE user_id = ?", response["account_id"])
+    rows = cur.fetchall()
+    ver_token = str(rows[0])
     
+    verify_url = f"http://{addr}:{port}/"
+    
+
     pass
 
 set_env()
